da790ed1d3ed85634cf17569db539d2adbc12815
phatcabbage <phatcabbage@gmail.com>
2017-04-23 21:26:54 -0400

    Remove ScriptCS submodule and replace it with NuGet references to ScriptCS packages.

--- build.sh
+++ build.sh
@@ -7,19 +7,6 @@ set -x
 mozroots --import --sync --quiet
 mono ./.nuget/NuGet.exe restore ./iCSharp.sln
 
-# Build scriptcs
-cd ./Engine
-
-# If brew is given as parameter, use the brew build-script
-if [ "$1" == "brew" ]
-then
-	./build_brew.sh
-else
-	./build.sh
-fi
-
-cd ../
-
 # Build iCSharp
 mkdir -p build/Release/bin
 xbuild ./iCSharp.sln /property:Configuration=Release /nologo /verbosity:normal
